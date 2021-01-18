f616e6889a2aa735e01a90662086c1e20804215f
ShyroVO <bosquet.laurie@outlook.com>
2021-01-18 13:50:15 +0100

    1.0

--- php.php
+++ php.php
@@ -1,12 +1,12 @@
 <?php
 
-    $texte = "Du texte est stocké ";
+    $texte = "Du texte est stocké <br>";
     $y = 30;
     $n = 5;
     $m = 7;
 
     echo $texte;
-    echo "y vaux $y ";
+    echo "y vaux $y <br>";
     echo $m + $n;
 
 ?>
